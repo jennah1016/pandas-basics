c46e7fa7e7c830a3aafde6a256a542be14d7495d
Jenna <104884851+jennah1016@users.noreply.github.com>
2024-11-13 21:39:03 -0600

    Add files via upload

--- read csv.py
+++ read csv.py
@@ -1,15 +1,15 @@
 import pandas as pd
 df = pd.read_csv('electric billing data.csv')
 
+#to-do: format to round to 2 decimal places
 avg2022 = df["2022"].mean()
 avg2023 = df['2023'].mean()
 avg2024YTD = df['2024 YTD'].mean()
 
+#to-do: round float values to 2 decimal places
 print('CTA electric billing data: January 2022 - September 2024:\n')
-#df['2024 YTD'].fillna('No Data', inplace=True)
 print (df.to_string())
 
-#to do: figure out how to concatenate numbers and strings
-'''print('\n 2022 Average: ' + avg2022 + '\n')
-print('2023 average: ' + avg2023 + '\n')
-print('2024 average: ' + avg2024YTD + '\n')'''
+print(f'\n2022 average : {avg2022}')
+print(f'2023 average: {avg2023}')
+print(f'2024 YTD average: {avg2024YTD}')
