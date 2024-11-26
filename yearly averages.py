d5c1edb59629c3ad8f9fc41295745bd5031c4496
Jenna <104884851+jennah1016@users.noreply.github.com>
2024-11-25 20:01:46 -0600

    Add files via upload

---
+++ yearly averages.py
@@ -0,0 +1,16 @@
+import pandas as pd
+df = pd.read_csv('electric billing data.csv')
+
+#average spending for each year
+avg2022 = df["2022"].mean()
+avg2023 = df['2023'].mean()
+avg2024YTD = df['2024 YTD'].mean()
+
+#displays monthly billing totals
+print('CTA electric billing data: January 2022 - September 2024:\n')
+print (df.to_string())
+
+#display average billing totals from each year
+print(f'\n2022 average : ${avg2022:.2f}')
+print(f'2023 average: ${avg2023:.2f}')
+print(f'2024 YTD average: ${avg2024YTD:.2f}')
